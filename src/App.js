aff24157031290ef773a3b26d508f0da894cdd32
Oleg <krasno1986@gmail.com>
2020-04-18 14:08:58 +0300

    add registration, login

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Registration from './Registration'
+import Login from './Login'
+import { BrowserRouter, Route, NavLink } from 'react-router-dom'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div>
+          <div>
+            <h2>Main menu</h2>
+            <div><NavLink activeStyle = {{color: 'red'}} to='/home'>HOME</NavLink></div>
+            <div><NavLink activeStyle = {{color: 'red'}} to='/registration'>registration</NavLink></div>
+            <div><NavLink activeStyle = {{color: 'red'}} to='/login'>log in</NavLink></div>
+          </div>
+          <div className="root">
+            <Route path = '/registration' component = {Registration}/>
+            <Route path = '/login' component = {Login}/>
+            <Route path = '/#' component = {App}/>
+          </div>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App;
+export default App
